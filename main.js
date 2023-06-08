27d413d89d947c7de1814fbddb28186392086e55
System Admin <systemadmin@MacBook-Pro-4512.local>
2023-06-07 22:59:38 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        icon: path.join(__dirname, 'icon.icns')
+    })
+
+    // Change the URL based on the environment variable
+    const url = process.env.NODE_ENV === 'dev' ? 'http://localhost:3000' : 'https://typescript-multiplayer-rpg.herokuapp.com/';
+    win.loadURL(url)
+}
+
+app.whenReady().then(createWindow)
